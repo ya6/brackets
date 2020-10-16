a036feb3f74080db6642b08a53746fc63a8f9267
YA <vector_cp@mail.ru>
2020-10-16 17:36:35 +0300

    init: make task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let made_optimization = true;
+
+  while (made_optimization) {
+      delta = arr.length;
+
+      for (let index = 0; index < arr.length; index += 1) {
+
+          bracketsConfig.forEach(element => {
+
+              if (element[0] === arr[index] && element[1] === arr[index + 1]) arr.splice(index, 2);
+
+          });
+      }
+      if (arr.length > 0 && delta > arr.length) made_optimization = true;
+      else made_optimization = false;
+  }
+
+  return arr.length === 0;
 }
